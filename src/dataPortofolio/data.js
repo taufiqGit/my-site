57d6070dfc4a1a20cfbaff94c59e7955ca5e0d08
taufiqGit <wecannn29@gmail.com>
2023-07-11 14:23:30 +0700

    add porto petapolitik

--- src/dataPortofolio/data.js
+++ src/dataPortofolio/data.js
@@ -3,7 +3,7 @@ const DataPortofolio = [
     {
         id: 1,
         title: 'Social media dashboard with theme switcher',
-        desc: 'asd vasd',
+        desc: 'lorem ipsum dolor.',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1651909053/portofolio/dashboard_ij6c6a.png',
         tags: ['#Tailwind', '#ReactJs'],
         link_demo: 'https://social-media-dashboard-ha.vercel.app/',
@@ -12,7 +12,7 @@ const DataPortofolio = [
     {
         id: 2,
         title: 'Manage Landing Page',
-        desc: 'asd asd',
+        desc: 'lorem ipsum dolor.',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1651908979/portofolio/managed_ahj9ud.png',
         tags: ['#Tailwind', '#Webpack', '#Responsive'],
         link_demo: 'https://manage-landing-page.taufiq.vercel.app/',
@@ -21,7 +21,7 @@ const DataPortofolio = [
     {
         id: 3,
         title: 'Advice generator app',
-        desc: 'asd asd',
+        desc: 'lorem ipsum dolor.',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1652006534/portofolio/Screenshot_from_2022-05-08_14-40-20_myhwwy.png',
         tags: ['#CSS', '#ReactJs'],
         link_demo: 'https://advice-generators.vercel.app/',
@@ -30,7 +30,7 @@ const DataPortofolio = [
     {
         id: 4,
         title: 'StoreGG (TopUp Voucher Game Site)',
-        desc: 'asd asd',
+        desc: 'lorem ipsum dolor.',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1652006970/portofolio/thumbnail_kelas_premium_Full-Stack_JavaScript_Developer_2021_buildwith_angga_1_2_1_xdazpc.png',
         tags: ['#Bootstrap', '#Typescript', '#NextJs', '#Responsive'],
         link_demo: 'https://storegg-th.vercel.app/',
@@ -39,7 +39,7 @@ const DataPortofolio = [
     {
         id: 5,
         title: 'Bookmark landing page',
-        desc: 'ASD asd',
+        desc: 'lorem ipsum dolor.',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1652007278/portofolio/Screenshot_from_2022-05-08_17-53-24_d812gy.png',
         tags: ['#SCSS', '#Webpack'],
         link_demo: 'https://bookmark-landing-page-taufiq.netlify.app/',
@@ -48,7 +48,7 @@ const DataPortofolio = [
     {
         id: 6,
         title: 'CMS Admin StoreGG',
-        desc: 'ASD asd',
+        desc: 'lorem ipsum dolor.',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1654060890/portofolio/hu_diafrv.png',
         tags: ['#Bootstrap', '#Express', "Mongoose"],
         link_demo: 'https://be-storegg.herokuapp.com/',
@@ -57,7 +57,7 @@ const DataPortofolio = [
     {
         id: 7,
         title: 'My Unsplash Gallery',
-        desc: 'ASD asd',
+        desc: 'lorem ipsum dolor.',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1654638199/portofolio/Screenshot_from_2022-06-08_04-38-09_pdzow0.png',
         tags: ['#ReactJs', '#Express', "Mongoose"],
         link_demo: 'https://my-unsplash-taufiq.netlify.app/',
@@ -66,7 +66,7 @@ const DataPortofolio = [
     {
         id: 8,
         title: 'Country Quiz App',
-        desc: 'ASD asd',
+        desc: 'lorem ipsum dolor.',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1653522072/portofolio/Screenshot_from_2022-05-26_06-36-57_ytopl6.png',
         tags: ['#ReactJs', '#Vite', "Redux"],
         link_demo: 'https://country-quiz-th.vercel.app/',
@@ -75,20 +75,29 @@ const DataPortofolio = [
     {
         id: 9,
         title: 'Shortly Link app',
-        desc: 'ASD asd',
+        desc: 'lorem ipsum dolor.',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1654642409/portofolio/Screenshot_from_2022-06-08_05-51-43_di1cew.png',
         tags: ['#ReactJs', '#TailwindCSS', "Context"],
         link_demo: 'https://shortly-react-app.taufiq.vercel.app/',
         link_repo: 'https://github.com/taufiqGit/Shortly-React-App'
     },
-        {
+    {
         id: 10,
         title: 'Weather App',
-        desc: 'ASD asd',
+        desc: 'lorem ipsum dolor.',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1663366610/portofolio/Screenshot_from_2022-09-17_05-15-33_vzr9bb.png',
         tags: ['#ReactJs', '#TailwindCSS', "Redux"],
         link_demo: 'https://weather-app-th.vercel.app/',
         link_repo: 'https://github.com/taufiqGit/weather-app'
+    },
+    {
+        id: 11,
+        title: 'PetaPolitik App',
+        desc: 'lorem ipsum dolor.',
+        thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1689059886/Screenshot_2023-07-11_141420_etrmup.jpg?_s=public-apps',
+        tags: ['#ReactJs', '#TailwindCSS', "Redux"],
+        link_demo: 'https://app.petapolitik.id',
+        link_repo: 'https://github.com/taufiqGit'
     }
 ]
 
