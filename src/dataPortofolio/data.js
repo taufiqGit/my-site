1cc5b309fbaba18733aeee52d6024c5c3af74af5
taufiqGit <wecannn29@gmail.com>
2023-09-28 21:03:37 +0700

    update porto

--- src/dataPortofolio/data.js
+++ src/dataPortofolio/data.js
@@ -3,7 +3,7 @@ const DataPortofolio = [
     {
         id: 1,
         title: 'Social media dashboard with theme switcher',
-        desc: 'lorem ipsum dolor.',
+        desc: '',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1651909053/portofolio/dashboard_ij6c6a.png',
         tags: ['#Tailwind', '#ReactJs'],
         link_demo: 'https://social-media-dashboard-ha.vercel.app/',
@@ -12,16 +12,52 @@ const DataPortofolio = [
     {
         id: 2,
         title: 'Manage Landing Page',
-        desc: 'lorem ipsum dolor.',
+        desc: '',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1651908979/portofolio/managed_ahj9ud.png',
         tags: ['#Tailwind', '#Webpack', '#Responsive'],
         link_demo: 'https://manage-landing-page.taufiq.vercel.app/',
         link_repo: 'https://github.com/taufiqGit/manage-landing-page'
     },
     {
+        id: 89986,
+        title: 'Tersistem',
+        desc: 'Tersistem is a legislative candidate application / legislative candidate team application which is a digital application resulting from a combined collaboration of several applications.',
+        thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1695904986/ijaijojk_qiboiq.webp',
+        tags: ['#Flutter', '#FlutterFlow', '#Dart'],
+        link_demo: 'https://play.google.com/store/apps/details?id=id.volume.android&pcampaignid=web_share',
+        link_repo: ''
+    },
+    {
+        id: 899233,
+        title: 'Relawan Perubahan',
+        desc: 'Change Volunteer Management Application for the 2024 Indonesian presidential election.',
+        thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1695905423/anice_zfrn8y.webp',
+        tags: ['#Flutter', '#FlutterFlow', '#Dart'],
+        link_demo: 'https://play.google.com/store/apps/details?id=id.reper.android&pcampaignid=web_share',
+        link_repo: ''
+    },
+    {
+        id: 229233,
+        title: 'Arita',
+        desc: 'Is a company website that operates in the import, distribution and service sectors for valve, fitting, instrumentation and control products.',
+        thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1695906357/Screenshot_2023-09-28_200512_fhyxyv.png',
+        tags: ['#Bootstrap', '#CSS', '#Javascript'],
+        link_demo: 'https://play.google.com/store/apps/details?id=id.reper.android&pcampaignid=web_share',
+        link_repo: ''
+    },
+    {
+        id: 311,
+        title: 'Gaweyo Admin Dashboard',
+        desc: 'Web application for Gaweyo admin and ad management',
+        thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1695908312/Screenshot_2023-09-28_203306-1_dcaz97.png',
+        tags: ['#Tailwind', '#ReactJs', '#Redux'],
+        link_demo: 'https://portal.gaweyo.com/portal-dashboard',
+        link_repo: ''
+    },
+    {
         id: 3,
         title: 'Advice generator app',
-        desc: 'lorem ipsum dolor.',
+        desc: '',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1652006534/portofolio/Screenshot_from_2022-05-08_14-40-20_myhwwy.png',
         tags: ['#CSS', '#ReactJs'],
         link_demo: 'https://advice-generators.vercel.app/',
@@ -30,16 +66,25 @@ const DataPortofolio = [
     {
         id: 4,
         title: 'StoreGG (TopUp Voucher Game Site)',
-        desc: 'lorem ipsum dolor.',
+        desc: '',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1652006970/portofolio/thumbnail_kelas_premium_Full-Stack_JavaScript_Developer_2021_buildwith_angga_1_2_1_xdazpc.png',
         tags: ['#Bootstrap', '#Typescript', '#NextJs', '#Responsive'],
         link_demo: 'https://storegg-th.vercel.app/',
         link_repo: 'https://github.com/taufiqGit/fe-storegg'
     },
     {
+        id: 11,
+        title: 'PetaPolitik App',
+        desc: '',
+        thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1689059886/Screenshot_2023-07-11_141420_etrmup.jpg?_s=public-apps',
+        tags: ['#ReactJs', '#TailwindCSS', "Redux"],
+        link_demo: 'https://app.petapolitik.id',
+        link_repo: 'https://github.com/taufiqGit'
+    },
+    {
         id: 5,
         title: 'Bookmark landing page',
-        desc: 'lorem ipsum dolor.',
+        desc: '',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1652007278/portofolio/Screenshot_from_2022-05-08_17-53-24_d812gy.png',
         tags: ['#SCSS', '#Webpack'],
         link_demo: 'https://bookmark-landing-page-taufiq.netlify.app/',
@@ -48,7 +93,7 @@ const DataPortofolio = [
     {
         id: 6,
         title: 'CMS Admin StoreGG',
-        desc: 'lorem ipsum dolor.',
+        desc: '',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1654060890/portofolio/hu_diafrv.png',
         tags: ['#Bootstrap', '#Express', "Mongoose"],
         link_demo: 'https://be-storegg.herokuapp.com/',
@@ -57,7 +102,7 @@ const DataPortofolio = [
     {
         id: 7,
         title: 'My Unsplash Gallery',
-        desc: 'lorem ipsum dolor.',
+        desc: '',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1654638199/portofolio/Screenshot_from_2022-06-08_04-38-09_pdzow0.png',
         tags: ['#ReactJs', '#Express', "Mongoose"],
         link_demo: 'https://my-unsplash-taufiq.netlify.app/',
@@ -66,7 +111,7 @@ const DataPortofolio = [
     {
         id: 8,
         title: 'Country Quiz App',
-        desc: 'lorem ipsum dolor.',
+        desc: '',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1653522072/portofolio/Screenshot_from_2022-05-26_06-36-57_ytopl6.png',
         tags: ['#ReactJs', '#Vite', "Redux"],
         link_demo: 'https://country-quiz-th.vercel.app/',
@@ -75,7 +120,7 @@ const DataPortofolio = [
     {
         id: 9,
         title: 'Shortly Link app',
-        desc: 'lorem ipsum dolor.',
+        desc: '',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1654642409/portofolio/Screenshot_from_2022-06-08_05-51-43_di1cew.png',
         tags: ['#ReactJs', '#TailwindCSS', "Context"],
         link_demo: 'https://shortly-react-app.taufiq.vercel.app/',
@@ -84,21 +129,12 @@ const DataPortofolio = [
     {
         id: 10,
         title: 'Weather App',
-        desc: 'lorem ipsum dolor.',
+        desc: '',
         thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/v1663366610/portofolio/Screenshot_from_2022-09-17_05-15-33_vzr9bb.png',
         tags: ['#ReactJs', '#TailwindCSS', "Redux"],
         link_demo: 'https://weather-app-th.vercel.app/',
         link_repo: 'https://github.com/taufiqGit/weather-app'
     },
-    {
-        id: 11,
-        title: 'PetaPolitik App',
-        desc: 'lorem ipsum dolor.',
-        thumbnail: 'https://res.cloudinary.com/djjavdrak/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1689059886/Screenshot_2023-07-11_141420_etrmup.jpg?_s=public-apps',
-        tags: ['#ReactJs', '#TailwindCSS', "Redux"],
-        link_demo: 'https://app.petapolitik.id',
-        link_repo: 'https://github.com/taufiqGit'
-    }
 ]
 
 export default DataPortofolio
